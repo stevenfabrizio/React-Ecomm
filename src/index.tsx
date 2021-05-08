b51f2bf12c5db75846bebf8beabb8b35b4015e5b
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-05-08 02:57:36 -0400

    initalizing repo

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactRouterDOM from 'react-dom';
+
+import App from './app'
+
+ReactRouterDOM.render(
+    <App />,
+    document.querySelector('#root-container')
+)
+
